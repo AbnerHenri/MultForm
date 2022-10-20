20c4e93a9e288050c7c062187783b03fee60aae0
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-10-19 22:27:09 -0300

    Add Provider on Application

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from './App'
 import './index.css'
 
 import { BrowserRouter } from 'react-router-dom'
+import { FormProvider } from './Contexts/FormContexts'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <FormProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FormProvider>
   </React.StrictMode>
 )
