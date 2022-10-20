8d80709ed06cad00f53076f152f6e9dcdba04592
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-10-19 22:11:16 -0300

    Create FormReducer

---
+++ src/Contexts/FormContexts.jsx
@@ -0,0 +1,63 @@
+import { createContext, useContext, useReducer } from 'react'
+
+const initialData = {
+    currentStep: 0,
+    name: '',
+    level: 0,
+    email: '',
+    github: ''
+}
+
+// Context
+
+const FormContext = createContext()
+
+// Reducer
+
+const FormReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_CURRENT_STEP':
+            return {...state, currentStep: action.payload}
+
+        case 'SET_NAME':
+            return {...state, name: action.payload}
+            break;
+
+        case 'SET_LEVEL':
+            return {...state, level: action.payload}
+
+        case 'SET_EMAIL':
+            return {...state, email: action.payload}
+
+        case 'SET_GITHUB':
+            return {...state, github: action.payload}
+    
+        default:
+            return state;
+    }
+}
+
+// Provider
+
+const FormProvider = ({ children }) => {
+
+    const [state, dispatch] = useReducer(FormReducer, initialData)
+    const value = { state, dispatch }
+
+    return(
+        <FormContext.Provider value={value}>
+            {children}
+        </FormContext.Provider>
+    )
+}
+
+// Context Hook
+
+const useForm = () => {
+    const context = useContext(FormContext)
+    if(context === undefined){
+        throw new Error('useForm precisa ser usado dentro do FormProvider')
+    }
+
+    return context
+}
