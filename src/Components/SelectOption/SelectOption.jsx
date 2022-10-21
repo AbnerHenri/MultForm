0da1707839723124874f41be53d1f0b3c7af9a26
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-10-21 17:51:56 -0300

    Create SelectOption Directory

---
+++ src/Components/SelectOption/SelectOption.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as C from './style'
+
+function SelectOption() {
+  return(
+    <C.Container>
+        
+    </C.Container>
+  );
+}
+
+export default SelectOption;
