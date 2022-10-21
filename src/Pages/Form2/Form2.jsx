a44442c9875d3826c6cb8a68a0d961fdef075a48
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-10-20 22:42:29 -0300

    Reacreated Form3

--- src/Pages/Form2/Form2.jsx
+++ src/Pages/Form2/Form2.jsx
@@ -1,14 +1,47 @@
-import Theme from '../../Components/Theme/Theme';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as C from './styles'
+import { useForm } from '../../Contexts/FormContexts'
 
-function FormTwo() {
+import Theme from '../../Components/Theme/Theme';
+
+function FormOne() {
+
+  const Redirect = useNavigate()
+  const { state, dispatch } = useForm()
+
+  useEffect(()=>{
+    dispatch({
+      type : 'SET_CURRENT_STEP',
+      payload : 2
+    })
+  }, [])
+
+  function nextStep(route){
+    if(state.name !== ''){
+      Redirect(route)
+    }else{
+      alert('Preencha os dados necessários')
+    }
+    
+  }
+
   return(
     <Theme>
       <C.Container>
-        Passo 2
+
+      <p>Passo {state.currentStep}/3</p>
+
+        <h1>Qual o seu nível de desenvolvedor</h1>
+      
+        <p>Escolha uma das opções abaixo</p>
+        <hr></hr>
+
+        <C.Button onClick={() => nextStep('/')}>Voltar</C.Button>
+        <C.Button onClick={() => nextStep('/step3')}>Próximo</C.Button>
       </C.Container>
     </Theme>
   );
 }
 
-export default FormTwo
+export default FormOne;
