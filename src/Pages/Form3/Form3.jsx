d5a56f326c31c995284e77d6911c4e01760da6b3
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-10-21 19:10:11 -0300

    Clear Form

--- src/Pages/Form3/Form3.jsx
+++ src/Pages/Form3/Form3.jsx
@@ -32,15 +32,12 @@ function FormOne() {
 
       <p>Passo {state.currentStep}/3</p>
 
-        <h1>Vamos começar com o seu nome</h1>
+        <h1>{state.name}, Vamos coletar os seus dados</h1>
         <p>Preencha o campo abaixo</p>
 
         <hr></hr>
 
-        <label>
-          Seu nome completo
-          <input type={'text'} onChange={handleName} autofocus />
-        </label>
+      
 
         <C.Button onClick={() => nextStep()}>Próximo</C.Button>
       </C.Container>
