1d108e979dafd57059ad39b8fe2795b96536d8bd
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-10-21 19:05:57 -0300

    Copy Form

---
+++ src/Pages/Form3/Form1.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as C from './styles'
+import { useForm } from '../../Contexts/FormContexts'
+
+import Theme from '../../Components/Theme/Theme';
+
+function FormOne() {
+
+  const Redirect = useNavigate()
+  const { state, dispatch } = useForm()
+
+  function nextStep(){
+    if(state.name !== ''){
+      Redirect('/step2')
+    }else{
+      alert('Preencha os dados necessários')
+    }
+    
+  }
+
+  function handleName(e){
+    dispatch({ 
+      type : 'SET_NAME',
+      payload : e.target.value
+    })
+  }
+
+  return(
+    <Theme>
+      <C.Container>
+
+      <p>Passo {state.currentStep}/3</p>
+
+        <h1>Vamos começar com o seu nome</h1>
+        <p>Preencha o campo abaixo</p>
+
+        <hr></hr>
+
+        <label>
+          Seu nome completo
+          <input type={'text'} onChange={handleName} autofocus />
+        </label>
+
+        <C.Button onClick={() => nextStep()}>Próximo</C.Button>
+      </C.Container>
+    </Theme>
+  );
+}
+
+export default FormOne;
--- src/Pages/Form3/styles.js
+++ src/Pages/Form3/styles.js
@@ -2,4 +2,55 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
 
+label {
+    font-size : 15px;
+
+    input {
+        display : block;
+        box-sizing: border-box;
+        width: 80%;
+        padding : 15px 10px;
+        margin-top : 10px;
+        margin-bottom: 15px;
+        border : 1px solid #ccc;
+        border-radius : 10px;
+        font-size: large;
+        color : #ccc;
+        outline: 0;
+        background-color: transparent;
+    }
+}
+
+h1 {
+    margin-bottom: 100px;
+}
+
+hr {
+    margin-bottom: 25px;
+}
+
+p {
+    margin : 0px 0px 15px 0px;
+    font-size: 20px;
+}
+`
+
+export const Button = styled.button`
+
+background-color: #449bac;
+color : #fff;
+
+border : none;
+border-radius: 30px;
+
+width : 150px;
+height : 50px;
+
+margin-top : 70px;
+margin-right: 10px;
+font-size : 16px;
+font-weight: 400;
+letter-spacing: 2px;
+
+cursor: pointer;
 `
