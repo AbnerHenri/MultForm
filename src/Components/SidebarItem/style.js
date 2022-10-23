613a1cc520e6b3d69a55ff498d4f926f0236a903
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-10-22 22:36:00 -0300

    Styled Info

--- src/Components/SidebarItem/style.js
+++ src/Components/SidebarItem/style.js
@@ -15,7 +15,13 @@ export const Container = styled.div`
 
 export const Info = styled.div`
 
-`
+    flex : 1;
+    margin-right : 30px;
+    text-align: right;
+    font-weight: 600;
+    margin-bottom: 5px;
+    font-size : 15px;
+`  
 
 export const Title = styled.div`
 
