87afacebd7b3650a4265c85d848567185c994ae4
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-10-22 22:37:45 -0300

    Styled Desc and Restyled Title

--- src/Components/SidebarItem/style.js
+++ src/Components/SidebarItem/style.js
@@ -17,18 +17,20 @@ export const Info = styled.div`
 
     flex : 1;
     margin-right : 30px;
+`  
+
+export const Title = styled.div`
     text-align: right;
     font-weight: 600;
     margin-bottom: 5px;
     font-size : 15px;
-`  
-
-export const Title = styled.div`
-
 `
 
 export const Desc = styled.div`
 
+    text-align : right;
+    font-size: 13px;
+    color : #b8b8d4;
 `
 
 export const IconArea = styled.div`
