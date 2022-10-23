e67d22915091c36625ff9551ffab505e8ac84a59
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-10-22 22:32:33 -0300

    Create Sidebar Component Structure

--- src/Components/SidebarItem/SidebarItem.jsx
+++ src/Components/SidebarItem/SidebarItem.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
-import * as C from './style'
 
-function SidebarItem() {
+import * as C from './style'
+import { Link } from 'react-router-dom'
+
+function SidebarItem(props) {
   return(
     <C.Container>
-        ...
+        <Link to={props.path}>
+            <C.Info>
+                <C.Title>{props.title}</C.Title>
+                <C.Desc>{props.description}</C.Desc>
+            </C.Info>
+
+            <C.IconArea>
+                <C.Point></C.Point>
+            </C.IconArea>
+        </Link>
     </C.Container>
   );
 }
--- src/Components/SidebarItem/style.js
+++ src/Components/SidebarItem/style.js
@@ -1,5 +1,25 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    
+
+`
+
+export const Info = styled.div`
+
+`
+
+export const Title = styled.div`
+
+`
+
+export const Desc = styled.div`
+
+`
+
+export const IconArea = styled.div`
+
+`
+
+export const Point = styled.div`
+
 `
